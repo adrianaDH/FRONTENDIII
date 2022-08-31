622c7a9183b2f514167ded462c56d938d05e61f8
Adriana Denise Fernandez <fernandez.adrianadenise@gmail.com>
2022-08-31 17:46:50 -0300

    entregable ok

---
+++ entregable-frontend3-agosto/README.md
@@ -0,0 +1,23 @@
+# Guía de consignas
+
+En cada uno de los componentes está detallado su funcionamiento.
+Ubicarse sobre la carpeta "entregable-frontend3-agosto"
+Ejecutar npm start desde la consola.
+
+# Demo del proyecto
+
+Aquí pueden ver una demo del funcionamiento de la aplicación.
+
+![demoReactFE3.gif](https://raw.githubusercontent.com/Frontend-III/entregable-frontend-3-junio22/main/demoReactFE3.gif)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ entregable-frontend3-agosto/src/App.js
@@ -0,0 +1,28 @@
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+
+import { useState } from "react";
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+
+function App() {
+
+  const [cantidadArticulos, setcantidadArticulos] = useState(0);
+  
+  const actualizarCantidad = () => setcantidadArticulos(cantidadArticulos + 1)
+  console.log('Incrementa cantidad articulos en App');
+
+  
+  return (
+    <div className="App">
+      <Cabecera cantidad={cantidadArticulos} />
+      <Listado comprarArticulo={actualizarCantidad} />
+    </div>
+  );
+}
+
+export default App;
