4fe9bdc1191722f2b2b6055545c13f1e154357d7
Adriana Denise Fernandez <fernandez.adrianadenise@gmail.com>
2022-08-31 22:26:54 -0300

    arreglo de tipeo en cabecera.js

--- entregable-frontend3-agosto/src/components/Cabecera.js
+++ entregable-frontend3-agosto/src/components/Cabecera.js
@@ -14,7 +14,7 @@ export default function Cabecera({cantidad}) {
   return (
     <header>
         <h1>Carrito de compras</h1>
-        <p>Cantidad de procutos: <span>{cantidad}</span></p>
+        <p>Cantidad de productos: <span>{cantidad}</span></p>
     </header>
   )
 }
